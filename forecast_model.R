3385c3b2b067ff3c9a8a4e38fe65b4ee7845e26b
Sarah Kanee <sarahkanee@gmail.com>
2026-03-26 16:36:40 -0400

    commented out aquatics data in attempts to resolve incessant error

--- forecast_model.R
+++ forecast_model.R
@@ -13,12 +13,12 @@ noaa_date <- Sys.Date() - days(3)  #Need to use yesterday's NOAA forecast becaus
 model_id <- "neon4cast_example"
 
 # Step 1: Download latest target data and site description data
-target <- readr::read_csv(paste0("https://data.ecoforecast.org/neon4cast-targets/",
-                                 "aquatics/aquatics-targets.csv.gz"), guess_max = 1e6)
-site_data <- readr::read_csv(paste0("https://raw.githubusercontent.com/eco4cast/neon4cast-targets/",
-                                    "main/NEON_Field_Site_Metadata_20220412.csv")) |> 
-  dplyr::filter(aquatics == 1)
-
+# target <- readr::read_csv(paste0("https://data.ecoforecast.org/neon4cast-targets/",
+#                                  "aquatics/aquatics-targets.csv.gz"), guess_max = 1e6)
+# site_data <- readr::read_csv(paste0("https://raw.githubusercontent.com/eco4cast/neon4cast-targets/",
+#                                     "main/NEON_Field_Site_Metadata_20220412.csv")) |> 
+#   dplyr::filter(aquatics == 1)
+# 
 
 # Step 2: Get meterological predictions as drivers
 df_past <- neon4cast::noaa_stage3()
