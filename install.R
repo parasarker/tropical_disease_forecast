b26a7cb717474914737c788b3a14ff3aaa3c246b
Sarah Kanee <sarahkanee@gmail.com>
2026-04-09 06:19:57 -0400

    updated install file

--- install.R
+++ install.R
@@ -1,3 +1,13 @@
 install.packages("remotes")   # if not already installed
 remotes::install_version("rMR", version = "1.1.0")
 install.packages("decor")
+
+# added from our scripts
+install.packages("readr")
+install.packages("ggplot2")
+install.packages("rgajs")
+install.packages("daymetr")#for now
+install.packages("ecoforecastR")
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("arrow")
